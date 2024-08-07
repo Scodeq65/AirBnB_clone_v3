847783933a0bbb3a02ff1fa29b44cba52ca70c49
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-07 14:00:44 +0100

    minor_correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,6 @@ Index file for API views
 
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
