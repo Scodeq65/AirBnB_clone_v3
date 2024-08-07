2bf88b609f52a23528bda078731240b7e194ce5f
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-07 02:55:04 +0100

    api_updated

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Flask application instance
+"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+"""
+the task module.
+"""
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    from os import getenv
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
+
---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Index file for API views
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
